ed076b2a55dc5546128401b7b25f1d4b61480313
Mateus Sousa <mateus.develp@gmail.com>
2019-04-19 21:32:23 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableNativeFeedback } from 'react-native';
+
+import Field from './src/component/Field'
+import params from './src/params';
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+  invertFlag,
+  flagsUsed
+} from './src/functions'
+import MineField from './src/component/MineField'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeeeeu!', 'Que buuuurro!')
+    }
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board, row, column)
+    const won = wonGame(board)
+
+    if (won) {
+      Alert.alert('Parabéns', 'Você Venceu!')
+    }
+
+    this.setState({ board, won })
+  }
+
+  onLevelSelected = level => {
+    params.difficultLevel = level
+    this.setState(this.createState())
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines!!</Text>
+        <Text style={styles.instructions}>Tamanho da Grade</Text>
+        <Text style={styles.instructions}>
+          {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <View style={styles.board} >
+          <MineField board={this.state.board} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+});
---
+++ src/component/MineField.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+import Field from './Field'
+
+export default props => {
+    const rows = props.board.map((row, r)=> {
+        const columns = row.map((field, c) => {
+            return <Field {...field} key={c} 
+                    onOpen={() => props.openField(r, c)}/>
+        })
+        return <View  style={{flexDirection: 'row'}} key={r}>{columns}</View>
+    })
+    return <View style={style.container} >{rows}</View>
+}
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: '#EEE'
+    }
+})
